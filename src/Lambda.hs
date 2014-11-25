393a55a2ab5c1677a1cb55e7415379478dc2ecee
Hugh FD Jackson <hughfdjackson@googlemail.com>
2014-11-25 19:53:11 +0000

    Inching closer

--- src/Lambda.hs
+++ src/Lambda.hs
@@ -1,76 +1,121 @@
 module Lambda (
   Expr(..),
   Name,
+  substitute,
   eval,
   isReducedForm,
-  v, l, ap, env,
+  v, l, ap,
   combI, combK,
-  showExpr
+  showExpr,
+  eval,
+  renameBoundTo,
+  renameBoundWithout,
+  freeNames,
+  names,
+  boundNames
 ) where
 
 import Prelude hiding (lookup)
 import Data.Map (Map, empty, insert, lookup, fromList, toList)
-import Data.List (intercalate)
+import Data.List (intercalate, elemIndex)
 import Data.Functor ((<$>))
-import Data.Maybe (fromMaybe)
+import Data.Maybe (fromMaybe, listToMaybe, fromJust)
+import qualified Data.Set as Set
 
 type Name = Char
 
 data Expr = V Name
-          | L Name Expr Env 
+          | L Name Expr
           | Ap Expr Expr
           deriving (Eq, Show)
 
 v name = V name
-l name expr = L name expr emptyEnv
+l name expr = L name expr
 ap expr expr' = Ap expr expr'
 
 showExpr :: Expr -> String
 showExpr expr = case expr of
   (V name)        -> showName name
   (Ap expr expr') -> showExpr expr ++ showExpr expr'
-  (L name expr env)   -> "(λ" ++ showName name ++ "." ++ showExpr expr ++ showEnv env ++ ")"
+  (L name expr)   -> "(λ" ++ showName name ++ "." ++ showExpr expr ++ ")"
 
+showName :: Name -> String 
+showName n = [n]
 
-showName :: Char -> String
-showName = (: [])
-
--- Environment 
-type Env = Map Name Expr
-
-emptyEnv :: Env
-emptyEnv = empty
-
-isEmptyEnv :: Env -> Bool
-isEmptyEnv e = e == emptyEnv
-
-env :: [(Name,Expr)] -> Env
-env list = fromList list
-
-showEnv :: Env -> String
-showEnv env
-  | isEmptyEnv env = ""
-  | otherwise      = "{" ++ (intercalate "," (formatPairs <$> (toList env))) ++ "}"
-  where formatPairs (name,expr) = showName name ++ "=" ++ showExpr expr
-
-eval :: Expr -> Expr
-eval = evalWithEnv emptyEnv
-
-evalWithEnv :: Env -> Expr -> Expr
-evalWithEnv env (Ap expr expr')  = let (L name body env') = evalWithEnv env expr
-                                       a                  = evalWithEnv env expr'
-                                       closureEnv         = insert name a env'
-                                   in evalWithEnv closureEnv body
-evalWithEnv env term@(V name) = lookup name env `getOrElse` term
-evalWithEnv env term@(L name body env') = L name body env
-  
 isReducedForm :: Expr -> Bool
 isReducedForm (Ap _ _) = False
 isReducedForm _        = True
 
-getOrElse :: Maybe a -> a -> a
-getOrElse = flip fromMaybe
-
 -- combinators
 combK = (l 'x' (l 'y' (v 'x')))
 combI = (l 'x' (v 'x'))
+
+eval :: Expr -> Expr
+eval (Ap inner@(Ap _ _) expr) = eval (Ap (eval inner) expr)
+eval (Ap inner@(L name body) expr) = substitute name expr body
+eval x = x
+
+substitute :: Name -> Expr -> Expr -> Expr
+substitute name arg expr = subIn cleanedExpr
+  where subIn = straightforwardSubstitute name arg 
+        blacklist = freeNames arg
+        cleanedExpr = foldl (flip (renameBoundWithout blacklist)) expr (Set.toList blacklist)
+
+
+straightforwardSubstitute :: Name -> Expr -> Expr -> Expr
+straightforwardSubstitute n e l@(L x expr)    = if x /= n then L x (straightforwardSubstitute n e expr) else l 
+straightforwardSubstitute n e (Ap expr expr') = Ap (straightforwardSubstitute n e expr) (straightforwardSubstitute n e expr')
+straightforwardSubstitute n e v@(V name)      = if name == n then e else v
+
+boundNames :: Expr -> Set.Set Name
+boundNames expr = boundNames' expr Set.empty
+  where boundNames' (L name expr)   names = Set.insert name names
+        boundNames' (Ap expr expr') names = Set.union (boundNames' expr names) (boundNames' expr' names)
+        boundNames' (V name)        names = names
+
+newName :: Name -> Expr -> Name
+newName = undefined
+
+freeNames :: Expr -> Set.Set Name
+freeNames expr = freeNames' expr Set.empty
+  where freeNames' (L name expr) names   = freeNames' expr (Set.insert name names)
+        freeNames' (Ap expr expr') names = Set.union (freeNames' expr names) (freeNames' expr' names)
+        freeNames' (V name) names        = if Set.member name names then Set.empty
+                                           else Set.singleton name
+
+names :: Expr -> Set.Set Name
+names expr = names' expr Set.empty
+  where names' (L n expr) names      = Set.union (Set.insert n names) (names' expr names)
+        names' (Ap expr expr') names = Set.union (names' expr names) (names' expr' names)
+        names' (V n)           names = Set.singleton n
+
+renameBoundWithout :: Set.Set Name -> Name -> Expr -> Expr
+renameBoundWithout blacklist name expr = renameBoundTo name next expr
+  where unavailable = Set.union (names expr) blacklist
+        available   = (Set.fromList ['a'..'z']) Set.\\ unavailable
+        next        = head $ cycle $ Set.toList available
+  
+renameBoundTo :: Name -> Name -> Expr -> Expr
+renameBoundTo name name' expr = renameBoundTo' expr
+  where renameBoundTo' l@(L name'' expr) = if name'' == name then renameAllVariablesTo name name' l
+                                         else L name'' (renameBoundTo' expr)
+        renameBoundTo' (Ap expr expr') = Ap (renameBoundTo' expr) (renameBoundTo' expr')
+        renameBoundTo' v = v
+                                                
+
+renameAllVariablesTo :: Name -> Name -> Expr -> Expr
+renameAllVariablesTo name name' = mapNames (\n -> if n == name then name' else n)
+
+mapNames :: (Name -> Name) -> Expr -> Expr
+mapNames fn (L name expr)   = L (fn name) (mapNames fn expr)
+mapNames fn (Ap expr expr') = Ap (mapNames fn expr) (mapNames fn expr')
+mapNames fn (V name)        = V (fn name)
+
+nextNameForExpr :: Name -> Expr -> Name
+nextNameForExpr name expr = newName
+  where banList   = Set.insert name $ names expr
+        nameCycle = cycle $ Set.toList $ Set.difference (Set.fromList ['a'..'z']) banList
+        oldNameIndex = elemIndex name nameCycle 
+        newName = head $ drop (fromMaybe 0 oldNameIndex) nameCycle
+
+
--- test/LambdaSpec.hs
+++ test/LambdaSpec.hs
@@ -3,24 +3,25 @@ module LambdaSpec (specs) where
 import Test.Hspec
 import Test.QuickCheck
 import Lambda
+import Data.Set as Set
 
-names :: Gen Char
-names = elements ['a'..'z']
+possibleNames :: Gen Char
+possibleNames = elements ['a'..'z']
 
 instance Arbitrary Expr where
   arbitrary = do
     n <- choose (0, 2) :: Gen Int
     case n of
-      0 -> do name <- names
+      0 -> do name <- possibleNames
               return $ v name
               
-      1 -> do name <- names
+      1 -> do name <- possibleNames
               expr <- arbitrary
               return $ l name expr
 
       2 -> do expr  <- arbitrary
               expr' <- arbitrary
-              name  <- names
+              name  <- possibleNames
               return $ ap (l name expr) expr'
   
 specs :: Spec
@@ -35,27 +36,61 @@ specs = describe "Lambda" $ do
     it "should show Abstraction" $ do
       showExpr (l 'x' (v 'y')) `shouldBe` "(λx.y)"
 
-    it "should show an L with env" $ do
-      showExpr (L 'x' (v 'y') (env [('y', V 'z')])) `shouldBe` "(λx.y{y=z})"
-
-    it "should show an L with an empty env just by skipping the env" $ do
-      showExpr (L 'x' (v 'y') (env [])) `shouldBe` "(λx.y)"
-      
-  describe "reduced form" $ do
+  describe "isReducedForm" $ do
     it "should tell whether a form is fulled reduced" $ do
       isReducedForm (ap (l 'x' (v 'x')) (v 'y')) `shouldBe` False
       isReducedForm (l 'x' (ap (v 'x') (v 'y'))) `shouldBe` True
       isReducedForm (v 'x')                      `shouldBe` True
 
-  describe "eval" $ do
-    it "should eval vars to themselves" $ do
-      eval (v 'x') `shouldBe` (v 'x')
+  describe "renameBoundTo" $ do
+    it "should rename to the next alphabetical letter *not* bound in the current expression" $ do
+      let xToA =  'x' `renameBoundTo` 'a'
+      let expr  = l 'x' (l 'y' (l 'z' (ap (ap (v 'x') (v 'y')) (v 'z'))))
+            
+      xToA expr `shouldBe` l 'a' (l 'y' (l 'z' (ap (ap (v 'a') (v 'y')) (v 'z'))))
 
-    it "should eval abstraction to itself" $ do
-      eval (l 'x' (v 'y')) `shouldBe` (l 'x' (v 'y'))
+    it "It should not rename a free variable" $ do
+      let xToA =  'x' `renameBoundTo` 'a'
+      let expr = l 'y' (ap (l 'x' (v 'x'))  (v 'x'))
+
+      xToA expr `shouldBe` l 'y' (ap (l 'a' (v 'a')) (v 'x'))
+
+  describe "renameBoundWithout" $ do
+    it "should renameBoundTo the first available name in the alphabet given an empty blacklist" $ do 
+      let expr  = l 'x' (l 'y' (l 'z' (ap (ap (v 'x') (v 'y')) (v 'z'))))
+      let expr' = l 'y' (ap (l 'x' (v 'x'))  (v 'x'))
+
+      renameBoundWithout Set.empty 'x' expr  `shouldBe` l 'a' (l 'y' (l 'z' (ap (ap (v 'a') (v 'y')) (v 'z'))))
+      renameBoundWithout Set.empty 'x' expr' `shouldBe` l 'y' (ap (l 'a' (v 'a')) (v 'x'))
+
+  describe "boundNames" $ do
+    it "should return all bound names" $ do
+      boundNames (l 'x' (ap (v 'y') (v 'x'))) `shouldBe` Set.singleton 'x'
+      boundNames (l 'x' (l 'y' (v 'x'))) `shouldBe` Set.singleton 'x'
+      boundNames (v 'y') `shouldBe` Set.empty
+
+  describe "freeNames" $ do
+    it "should return all free names" $ do
+       freeNames (l 'x' (ap (v 'y') (v 'x'))) `shouldBe` Set.singleton 'y'
+       freeNames (v 'y') `shouldBe` Set.singleton 'y'
+       freeNames (ap (v 'y') (v 'x')) `shouldBe` Set.fromList ['y', 'x']
+
+  describe "names" $ do
+    it "should return full list of all names, free or bound" $ do
+       names (l 'x' (ap (v 'y') (v 'x'))) `shouldBe` Set.fromList ['y', 'x']
+       names (v 'y') `shouldBe` Set.singleton 'y'
+       names (ap (v 'y') (v 'x')) `shouldBe` Set.fromList ['y', 'x']
       
-    it "should evaluate identity fn to obey identity law" $ property $ \expr ->
-      isReducedForm expr ==> eval (ap combI expr) === (expr :: Expr)
-      
-    it "should evaluate constant fn to obey constant law" $ property $ \expr ->
-      isReducedForm expr ==> eval (ap (ap combK expr ) (v 'z')) === (expr :: Expr)
+  describe "substitute" $ do
+    it "should substitute a free variable in an expression" $ do
+      let expr = l 'x' (ap (v 'y') (v 'x'))
+      substitute 'y' (v 'z') expr `shouldBe` l 'x' (ap (v 'z') (v 'x'))
+
+    it "should re-name variables in by using the same logic as renameBound" $ do
+      let expr = l 'x' (ap (v 'a') (v 'x'))
+      substitute 'a' (v 'x') expr `shouldBe` l 'b' (ap (v 'x') (v 'b'))
+
+  describe "evaluate" $ do
+    it "should .. evaluate things!" $ do 
+      let expr = (ap (l 'x' (ap (v 'y') (v 'x'))) (v 'a'))
+      eval expr `shouldBe` (ap (v 'y') (v 'a'))
